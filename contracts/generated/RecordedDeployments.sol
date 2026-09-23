d8487f9cfd8ff80bb486aa31c84268f5151953b4
Michael Heuer <michael.heuer@rwth-aachen.de>
2026-09-24 00:49:51 +0200

    chore: redeployed to sepolia

--- contracts/generated/RecordedDeployments.sol
+++ contracts/generated/RecordedDeployments.sol
@@ -42,7 +42,8 @@ library RecordedDeployments {
     /// @notice Returns the deployments the staging environment records.
     /// @return deployments The recorded staging deployments.
     function staging() internal pure returns (Deployment[] memory deployments) {
-        deployments = new Deployment[](0);
+        deployments = new Deployment[](1);
+        deployments[0] = Deployment({chainId: 11155111, contractAddress: 0x4d3342bf4975ac8d325087FE631357679a0E3c82});
     }
 
     /// @notice Returns the deployments the production environment records.
